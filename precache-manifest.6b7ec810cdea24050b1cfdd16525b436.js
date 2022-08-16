191dc880cf2e4c072e7a792c8f73b885ae0e042b
Manish Savier <manishsavier5@gmail.com>
2022-08-16 18:09:01 +0530

    Updates

---
+++ precache-manifest.6b7ec810cdea24050b1cfdd16525b436.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a45bc3cde0714aaf504",
+    "url": "/keep-notes-project/static/js/main.6a45bc3c.chunk.js"
+  },
+  {
+    "revision": "3e986c39d9d8945ea43b",
+    "url": "/keep-notes-project/static/js/1.3e986c39.chunk.js"
+  },
+  {
+    "revision": "29c3d2521e47d03bbb4b",
+    "url": "/keep-notes-project/static/js/runtime~main.29c3d252.js"
+  },
+  {
+    "revision": "62baa2bbda9f689ee2a82ef4ba5a2407",
+    "url": "/keep-notes-project/index.html"
+  }
+];
